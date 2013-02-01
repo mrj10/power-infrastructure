ed142ad2d2fdc0baad64e579e5b6d1402089bd55
Matt Johnson <johnso87@illinois.edu>
2013-02-01 13:15:09 -0600

    Add getopts to set_freq.sh, can now optionally not save previous settings

--- set_freq.sh
+++ set_freq.sh
@@ -1,15 +1,41 @@
 #!/bin/sh
 NUMPROCS=$(grep -c ^processor /proc/cpuinfo)
 CPUFREQPREFIX=/sys/devices/system/cpu
-SAVEPREFIX=/tmp/set_freq/previous_
-if [ $# != 1 ]
+SAVEDIR=/tmp/set_freq
+SAVEPREFIX=$SAVEDIR/previous
+SAVE=1
+
+while getopts s:rn o
+do	case "$o" in
+	s)	if [ "$OP" = "reset" ]
+                then
+                  echo "Error: Cannot specify -s and -r" >&2
+                  exit 1
+                fi
+                OP="set"
+                FREQ="$OPTARG";;
+	r)	if [ "$OP" = "set" ]
+                then
+                  echo "Error: Cannot specify -s and -r" >&2
+                  exit 1
+                fi
+                OP="reset";;
+        n)      SAVE=0;;
+	[?])	echo "Usage: $0 [-s freq_in_khz] [-n] [-r]" >&2
+		exit 1;;
+	esac
+done
+
+if [ -z $OP ]
 then
-  echo "Usage: $0 <frequency in kHz>"
-  echo "       $0 reset"
+  echo "Usage: $0 [-s freq_in_khz] [-n] [-r]" >&2
   exit 1
 fi
 
-if [ $1 = "reset" ]
+# Reset $@
+shift `echo $OPTIND-1 | bc`
+
+if [ $OP = "reset" ]
 then
   for i in $(seq 0 $(expr $NUMPROCS - 1))
   do
@@ -17,25 +43,35 @@ then
     sudo cp ${SAVEPREFIX}_min_$i $CPUFREQPREFIX/cpu$i/cpufreq/scaling_min_freq
     sudo cp ${SAVEPREFIX}_max_$i $CPUFREQPREFIX/cpu$i/cpufreq/scaling_max_freq
   done
-  echo "Reset all cores to previous frequency setting"
+  echo "Reset all $NUMPROCS cores to previous frequency setting"
   exit 0
 fi
 
-for freq in $(cat $CPUFREQPREFIX/cpu0/cpufreq/scaling_available_frequencies)
+mkdir -p $SAVEDIR
+for freq2 in $(cat $CPUFREQPREFIX/cpu0/cpufreq/scaling_available_frequencies)
 do
-  if [ $1 = $freq ]
+  if [ $FREQ = $freq2 ]
   then
     for i in $(seq 0 $(expr $NUMPROCS - 1))
     do
-      cp $CPUFREQPREFIX/cpu$i/cpufreq/scaling_governor ${SAVEPREFIX}_governor_$i
-      cp $CPUFREQPREFIX/cpu$i/cpufreq/scaling_min_freq ${SAVEPREFIX}_min_$i
-      cp $CPUFREQPREFIX/cpu$i/cpufreq/scaling_max_freq ${SAVEPREFIX}_max_$i
+      if [ $SAVE = 1 ]
+      then
+        cp $CPUFREQPREFIX/cpu$i/cpufreq/scaling_governor ${SAVEPREFIX}_governor_$i
+        cp $CPUFREQPREFIX/cpu$i/cpufreq/scaling_min_freq ${SAVEPREFIX}_min_$i
+        cp $CPUFREQPREFIX/cpu$i/cpufreq/scaling_max_freq ${SAVEPREFIX}_max_$i
+      fi
       echo "userspace" | sudo tee $CPUFREQPREFIX/cpu$i/cpufreq/scaling_governor >/dev/null
-      echo "$1" | sudo tee $CPUFREQPREFIX/cpu$i/cpufreq/scaling_setspeed >/dev/null
+      echo "$FREQ" | sudo tee $CPUFREQPREFIX/cpu$i/cpufreq/scaling_setspeed >/dev/null
     done
-    echo "Set all cores to $1 kHz, stored previous settings in ${SAVEPREFIX}_*"
+    echo -n "Set all $NUMPROCS cores to $FREQ kHz, "
+    if [ $SAVE = 1 ]
+    then
+      echo "stored previous settings in ${SAVEPREFIX}_*"
+    else
+      echo "did not save previous settings"
+    fi
     exit 0
   fi
 done
-echo "Failure: '$1' is not one of the allowed frequencies: $(cat $CPUFREQPREFIX/cpu0/cpufreq/scaling_available_frequencies)"
+echo "Failure: '$FREQ' is not one of the allowed frequencies: $(cat $CPUFREQPREFIX/cpu0/cpufreq/scaling_available_frequencies)"
 exit 1
